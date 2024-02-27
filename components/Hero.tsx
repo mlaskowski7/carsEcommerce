6e062038459f3d628fe37de9a70fd9a48282b11f
Mateusz Laskowski <mlaskowski7>
2024-02-27 23:52:45 +0100

    designing navbar and footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,8 @@ const Hero = () => {
 
         <div className="hero__image-container">
           <div className="hero__image">
-            <Image src=/>
+            <Image src="/hero.png" alt="hero image" fill className="object-contain" />
+            <div className="hero__image-overlay" />
           </div>
         </div>
       </div>
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,7 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
-export { Hero, CustomButton };
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+
+export { Hero, CustomButton, Footer, Navbar };
