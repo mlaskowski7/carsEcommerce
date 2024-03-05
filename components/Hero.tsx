26d4fac4170ca7fb36234d8ada92fbe8df2e23a4
Mateusz Laskowski <mlaskowski7>
2024-03-05 22:10:27 +0100

    commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
   const handleScroll = () =>{};
 
   return (
-    <div className="hero">
+    <div className="hero"> 
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
           Find, book or rent a car - quickly and easily!
@@ -21,17 +21,16 @@ const Hero = () => {
 
         <CustomButton 
           title = "Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10 handleClick={}"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10 handleClick={}" 
           handleClick={handleScroll}
         />
-
-        <div className="hero__image-container">
+      </div>
+      <div className="hero__image-container">
           <div className="hero__image">
             <Image src="/hero.png" alt="hero image" fill className="object-contain" />
-            <div className="hero__image-overlay" />
           </div>
+          <div className="hero__image-overlay" />
         </div>
-      </div>
     </div>
   );
 };
